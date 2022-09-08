c0d89c9a63acb930c6e53de0db9c8f0bd98f8f98
Kashu-jnr <kashueric63@gmail.com>
2022-09-08 07:48:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -15,9 +15,16 @@
 * Return: 0
 
 */
+
 int main(void)
+
 {
-     puts("\"Programming is like building a multilingual puzzle");
-       
-     Return:0;
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
+
+
+
